52711cbd93dd8a13ac27f9382e3d77b9c04f9498
SeanLiu8800 <tyson9963@gmail.com>
2026-08-11 22:01:19 -0700

    Defined some preliminary Game Events

---
+++ Assets/Scripts/GameManager/GameEventsStruct.cs
@@ -0,0 +1,9 @@
+using UnityEngine;
+using System;
+
+public struct GameEventsStruct
+{
+    public Action duskStarts;
+    public Action dawnStarts;
+    public Action dayStarts;
+}
--- Assets/Scripts/GameManager/GameManager.cs
+++ Assets/Scripts/GameManager/GameManager.cs
@@ -9,10 +9,15 @@ public class GameManager : MonoBehaviour
     [field: SerializeField, Range(0.0f, 360.0f)] public float dawnTimeLength { get; private set; } = 120.0f;
     [field: SerializeField, Range(0.0f, 360.0f)] public float dayTimeLength { get; private set; } = 120.0f;
 
+    public GameEventsStruct gameEvents;
     public static GameManager instance;
     private void Awake()
     {   // Establish static reference
-        if (GameManager.instance != null && GameManager.instance != this) Debug.LogError("Another GameManager tried to Instantiate! Deleting!");
+        if (GameManager.instance != null && GameManager.instance != this)
+        {
+            Debug.LogError("Another GameManager tried to Instantiate! Deleting!");
+            Destroy(this.gameObject);
+        }
         else GameManager.instance = this;
     }
     private void OnDestroy()
