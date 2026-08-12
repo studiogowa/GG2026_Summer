433be02e0e7b613c4db8ea33eb75a3d6ebe57bdb
SeanLiu8800 <tyson9963@gmail.com>
2026-08-11 22:20:52 -0700

    Wrote down preliminary functions
    -Created a GameState enum to encapsulate Dusk, Dawn and Day state
    -Created variables to keep track of Round Time

--- Assets/Scripts/GameManager/GameManager.cs
+++ Assets/Scripts/GameManager/GameManager.cs
@@ -5,9 +5,16 @@ using System.Collections.Generic;
 public class GameManager : MonoBehaviour
 {
     [Header("Game Progression Variables")]
-    [field: SerializeField, Range(0.0f, 360.0f)] public float duskTimeLength { get; private set; } = 120.0f;
-    [field: SerializeField, Range(0.0f, 360.0f)] public float dawnTimeLength { get; private set; } = 120.0f;
-    [field: SerializeField, Range(0.0f, 360.0f)] public float dayTimeLength { get; private set; } = 120.0f;
+    [field: SerializeField, Range(0.0f, 360.0f)] public float duskRoundTime { get; private set; } = 120.0f;
+    [field: SerializeField, Range(0.0f, 360.0f)] public float dawnRoundTime { get; private set; } = 120.0f;
+    [field: SerializeField, Range(0.0f, 360.0f)] public float dayRoundTime { get; private set; } = 120.0f;
+
+    [field: SerializeField] public GameState gameState = GameState.Dusk;
+
+    public float gameStartTime { get; private set; } = 0.0f;
+    public float gameEndTime { get; private set; } = 0.0f;
+    public float gameRoundTime { get { return Time.time - gameStartTime; } }
+    public float gameTimeRemaining { get { return gameEndTime - gameStartTime; } }
 
     public GameEventsStruct gameEvents;
     public static GameManager instance;
@@ -24,4 +31,49 @@ public class GameManager : MonoBehaviour
     {   // Remove static reference
         if (GameManager.instance != null && GameManager.instance == this) GameManager.instance = null;
     }
+
+    private void StartDuskRound()
+    {
+        gameState = GameState.Dusk;
+
+        gameStartTime = Time.time;
+        gameEndTime = gameStartTime + duskRoundTime;
+
+        gameEvents.duskStarts?.Invoke();
+    }
+
+    private void StartDawnRound()
+    {
+        gameState = GameState.Dawn;
+
+        gameStartTime = Time.time;
+        gameEndTime = gameStartTime + dawnRoundTime;
+
+        gameEvents.dawnStarts?.Invoke();
+    }
+
+    private void StartDayRound()
+    {
+        gameState = GameState.Day;
+
+        gameStartTime = Time.time;
+        gameEndTime = gameStartTime + dayRoundTime;
+
+        gameEvents.duskStarts?.Invoke();
+    }
+
+    private void EndRound()
+    {
+        switch (gameState)
+        {
+            case GameState.Dusk:
+                break;
+            case GameState.Dawn:
+                break;
+            case GameState.Day:
+                break;
+        }
+    }
 }
+
+public enum GameState { Dusk, Dawn, Day }
