390cb04c50912514c8dfc03a00ac022425af4ca1
SeanLiu8800 <tyson9963@gmail.com>
2026-08-12 15:19:42 -0700

    Formalized Dusk/ Dawn/ Day loop

--- Assets/Scripts/GameManager/GameManager.cs
+++ Assets/Scripts/GameManager/GameManager.cs
@@ -31,6 +31,10 @@ public class GameManager : MonoBehaviour
     {   // Remove static reference
         if (GameManager.instance != null && GameManager.instance == this) GameManager.instance = null;
     }
+    private void Start()
+    {
+        StartDuskRound();
+    }
 
     private void StartDuskRound()
     {
@@ -40,6 +44,8 @@ public class GameManager : MonoBehaviour
         gameEndTime = gameStartTime + duskRoundTime;
 
         gameEvents.duskStarts?.Invoke();
+        Debug.Log("Dusk Round Starts!");
+        trackRoundProgressCoroutine = StartCoroutine(TrackRoundProgress());
     }
 
     private void StartDawnRound()
@@ -50,6 +56,8 @@ public class GameManager : MonoBehaviour
         gameEndTime = gameStartTime + dawnRoundTime;
 
         gameEvents.dawnStarts?.Invoke();
+        Debug.Log("Dawn Round Starts!");
+        trackRoundProgressCoroutine = StartCoroutine(TrackRoundProgress());
     }
 
     private void StartDayRound()
@@ -60,17 +68,32 @@ public class GameManager : MonoBehaviour
         gameEndTime = gameStartTime + dayRoundTime;
 
         gameEvents.duskStarts?.Invoke();
+        Debug.Log("Day Round Starts!");
+        trackRoundProgressCoroutine = StartCoroutine(TrackRoundProgress());
     }
-
+    private Coroutine trackRoundProgressCoroutine;
+    private IEnumerator TrackRoundProgress()
+    {
+        // Track round time progress
+        while (Time.time < gameEndTime) // Time.time is affected by Time.timescale!
+        {
+            yield return null;
+        }
+        // Round Ends
+        EndRound();
+    }
     private void EndRound()
     {
         switch (gameState)
         {
             case GameState.Dusk:
+                StartDawnRound();
                 break;
             case GameState.Dawn:
+                StartDayRound();
                 break;
             case GameState.Day:
+                StartDuskRound();
                 break;
         }
     }
