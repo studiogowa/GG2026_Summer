d78542c76141e6d15e2be64686529212a3969573
SeanLiu8800 <tyson9963@gmail.com>
2026-08-21 17:31:40 -0700

    Prepared a primitive animation for PerfRev

--- Assets/Scripts/UIScripts/PerformanceReview.cs
+++ Assets/Scripts/UIScripts/PerformanceReview.cs
@@ -1,6 +1,33 @@
 using UnityEngine;
-
+using UnityEngine.InputSystem;
 public class PerformanceReview : GameUIComponent
 {
-    
+    private Animator animator;
+    protected override void Awake()
+    {
+        base.Awake();
+        if (!TryGetComponent<Animator>(out animator)) Debug.LogError($"{this.name} DOES NOT have an animator component!");
+    }
+    private void Update()
+    {
+        if (Keyboard.current.digit1Key.wasPressedThisFrame) ToggleMenu();
+    }
+    private bool menuOpened = false;
+    private void ToggleMenu()
+    {
+        if (!menuOpened) OpenMenu();
+        else CloseMenu();
+    }
+    private void OpenMenu()
+    {
+        if (menuOpened) return;
+        menuOpened = true;
+        animator.SetTrigger("Open");
+    }
+    private void CloseMenu()
+    {
+        if (!menuOpened) return;
+        menuOpened = false;
+        animator.SetTrigger("Close");
+    }
 }
