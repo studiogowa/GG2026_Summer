09cedca08bad0db17497ffa38f952156e1e843ec
DESKTOP-VV8EQIK\Beatrice <beatrice.chan1224@live.com>
2026-08-28 14:09:28 -0700

    Set foods and potions to stackable Added value and description field to items

--- Assets/Scripts/Items/Item.cs
+++ Assets/Scripts/Items/Item.cs
@@ -4,8 +4,10 @@ using UnityEngine;
 public class Item : ScriptableObject
 {
     new public string name = "New Item";
+    public string description = "";
     public Sprite icon = null;
     public int amount;
+    public int value;
     public bool isStackable = false;
     public bool isDefaultItem = false;
     public GameObject prefab;
